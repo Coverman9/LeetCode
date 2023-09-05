340bee178f1551c6d47a3533bc280e703c00b23a
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-09-05 10:19:58 -0500

    Time: 49 ms (95.91%) | Memory: 42.5 MB (52.06%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -7,7 +7,7 @@ var twoSum = function(nums, target) {
     let mp = new Map()
     
     for (let i = 0; i < nums.length; i++) {
-        let diff =  target - nums[i]
+        let diff = target - nums[i]
         
         if (mp.has(diff)) {
             return [i, mp.get(diff)]
