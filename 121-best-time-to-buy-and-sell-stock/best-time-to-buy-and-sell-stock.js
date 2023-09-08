b45a6f050460935ccf0609c91e9a96b220811444
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-09-08 16:37:09 -0500

    Time: 84 ms (25.25%) | Memory: 51.8 MB (19.69%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    // let min = Math.min(...prices)
+    // let max = Math.max(...prices)
+    
+    // let bought = 0;
+    // let maxBought = 0
+    // for (let i = 0; i < prices.length; i++) {
+    //     for (let j = i + 1; j < prices.length; j++) {
+    //         bought = prices[j] - prices[i]
+    //         if (bought > maxBought) {
+    //             maxBought = bought
+    //         }
+    //     }
+    // }
+
+    // for (let i = 0; i < prices.length; i++) {
+    //     let max = Math.max(...prices.slice(i))
+    //     bought = max - prices[i]
+    //     if (bought > maxBought) {
+    //         maxBought = bought
+    //     }
+        
+    // }
+    // return maxBought
+    
+    let profit = 0
+    let buy = prices[0]
+    for (let i = 0; i < prices.length; i++) {
+        let sell = prices[i]
+        if (sell > buy) {
+            profit = Math.max(profit, sell - buy)
+        } else {
+            buy = sell
+        }
+    }
+    return profit
+    
+
+
+};
