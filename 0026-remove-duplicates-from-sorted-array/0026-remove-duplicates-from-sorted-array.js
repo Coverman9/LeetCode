78dd70a91c793eda6d12585d58c45b94630c3b1d
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-09-19 15:04:56 -0500

    Time: 69 ms (43.34%), Space: 44.5 MB (58.19%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let set = new Set(nums);
+    nums.length = 0;
+    nums.push(...set.values())
+    return set.size;
+};
