e9982d3c12efdfaa8c0fb77b2d8b9456e745bfc5
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-10-02 16:33:35 -0500

    Time: 49 ms (70.88%), Space: 41.7 MB (64.93%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -4,8 +4,6 @@
  */
 var plusOne = function(digits) {
     let res = digits.join('')
-    console.log(res)
     res = BigInt(res) + BigInt(1)
-    console.log(res)
     return res.toString().split('')
 }
