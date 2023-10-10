eb5d2b8a92582201393caa1ee595ef55dbeb866a
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-10-10 01:56:34 -0500

    Time: 67 ms (12.65%), Space: 45 MB (8.64%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let res = init
+    
+    return {
+        increment:() => ++res,
+        reset:() => res = init, 
+        decrement:(n) => --res
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
