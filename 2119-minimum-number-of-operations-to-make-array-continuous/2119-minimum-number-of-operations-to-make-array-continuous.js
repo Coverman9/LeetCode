cf4f711ca773d103c8f419172278101ef60f7c9f
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-10-09 23:54:01 -0500

    Time: 218 ms (62.07%), Space: 74.9 MB (68.97%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.js
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.js
@@ -3,36 +3,17 @@
  * @return {number}
  */
 var minOperations = function(nums) {
-    nums.sort((a,b) => a - b)
-    let n = nums.length;
-    nums = [...new Set(nums)]; 
+    let n = nums.length, res = n;
+    const numsU = Array.from(new Set(nums));
+    let m = numsU.length;
+    numsU.sort((a, b) => a - b);
 
-    let ans = Number.MAX_SAFE_INTEGER;
-
-    for (let i = 0; i < nums.length; i++) {
-        let s = nums[i];
-        let e = s + n - 1;
-        let idx = binarySearch(nums, e);
-
-        ans = Math.min(ans, n - (idx - i));
-    }
-
-    return ans;
-};
-
-function binarySearch(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-
-    while (left <= right) {
-        let mid = left + Math.floor((right - left) / 2);
-
-        if (nums[mid] <= target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+    for(let i = 0, j = 0; i < m; ++i) {
+        while(j < m && numsU[j] < numsU[i] + n) {
+            ++j;
         }
+        res = Math.min(res, n - j + i);
     }
 
-    return left;
+    return res;
 }
