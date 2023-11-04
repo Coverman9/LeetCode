a98efef9fea37ddea5a28c36997f988b86e4a5d5
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-11-04 18:03:48 -0500

    Time: 73 ms (82.69%) | Memory: 54.7 MB (45.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,17 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let obj = {}
-    for (let el of nums) {
-        if (obj[el]) {
-            obj[el]++
-        } else {
-            obj[el] = 1
-        }
-    }
-
-    for (let nums of Object.values(obj)) {
-        if (nums >= 2) return true
-    }
-    return false
+    let set = new Set(nums)
+    return set.size !== nums.length
 };
