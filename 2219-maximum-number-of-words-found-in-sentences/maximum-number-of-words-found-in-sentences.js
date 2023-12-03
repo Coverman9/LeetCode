d2f26ba89e32a3f1a7805ffdc657a1fdc061d11b
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-12-03 16:48:33 -0600

    Time: 63 ms (15.40%) | Memory: 44.5 MB (26.22%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
@@ -5,11 +5,7 @@
 var mostWordsFound = function(sentences) {
     let res = []
     for (let el of sentences) {
-        res.push(el.split(' '))
+        res.push(el.split(' ').length)
     }
-    let nums = []
-    for (let sent of res) {
-        nums.push(sent.length)
-    }
-    return Math.max(...nums)
+    return Math.max(...res)
 };
