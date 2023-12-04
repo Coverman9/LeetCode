fe72651807aa61d7d0171acdc60271f1f2bbee1d
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-12-04 01:02:56 -0600

    Time: 60 ms (36.56%) | Memory: 45.9 MB (9.68%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.js
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.js
@@ -3,13 +3,16 @@
  * @return {string}
  */
 var largestGoodInteger = function(num) {
-    const matches = [
-        '999', '888', '777', '666', '555', '444', '333', '222', '111', '000'
-    ];
-    for (const match of matches) {
-        if (num.includes(match)) {
-            return match;
-        } 
+    let res = []
+    let curr = ''
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] === num[i + 1] && num[i] === num[i + 2]) {
+            curr += num[i] + num[i + 1] + num[i + 2]
+        }
+        if (curr.length === 3) {
+            res.push(Number(curr))
+            curr = ''
+        }
     }
-    return '';
+    return res.length ? Math.max(...res).toString().padStart(3, '0') : ''
 };
