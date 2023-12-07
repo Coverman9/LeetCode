5c5236fd1d08a205d289672435e07e8ad4963cd6
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-12-06 22:35:26 -0600

    Time: 50 ms (94.83%) | Memory: 45.3 MB (71.47%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -3,12 +3,10 @@
  * @return {string}
  */
 var largestOddNumber = function(num) {
-    let curr = ''
     for (let i = num.length - 1; i >= 0; i--) {
        if (Number(num[i]) % 2 !== 0) {
-           curr = num.slice(0, i + 1)
-           break
+           return num.slice(0, i + 1)
        } 
     }
-    return curr 
+    return ''
 };
