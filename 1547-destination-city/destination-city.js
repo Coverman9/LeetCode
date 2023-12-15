0aa97a94ec6d5eb2e789cf4d1042a580d5817a9f
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-12-15 12:27:20 -0600

    Time: 60 ms (39.87%) | Memory: 45 MB (16.94%) - LeetSync

---
+++ 1547-destination-city/destination-city.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function(paths) {
+    const cities = new Set();
+
+    for (const path of paths) {
+        cities.add(path[0]);
+    }
+    console.log(cities)
+        
+    for (const path of paths) {
+        const dest = path[1];
+        if (!cities.has(dest)) {
+            return dest;
+        }
+    }
+
+        return "";
+};
