6c7ea0dc67e6f0777e76afa7517788547649e35f
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-12-21 13:57:01 -0600

    Time: 48 ms (79.42%) | Memory: 42.2 MB (36.14%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -3,19 +3,14 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    for (let i=0;i<matrix.length;i++) {
-        for (let j=i;j<matrix[0].length;j++) {
+     for(let i = 0; i < matrix.length; i += 1) {
+        for(let j = i + 1; j < matrix.length; j += 1) {
             let temp = matrix[i][j];
             matrix[i][j] = matrix[j][i];
             matrix[j][i] = temp;
         }
-    }
-
-    for (let i=0;i<matrix.length;i++) {
-        for (let j=0;j<matrix[0].length/2;j++) {
-            let temp = matrix[i][j];
-            matrix[i][j] = matrix[i][matrix[0].length-j-1];
-            matrix[i][matrix[0].length-j-1] = temp;
-        }
+    } 
+    for(let row of matrix) {
+        row.reverse();
     }
 };
