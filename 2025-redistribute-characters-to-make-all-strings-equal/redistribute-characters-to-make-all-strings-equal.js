ed88b50d3e244093f22ddb1759eae38fee6b95e1
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2023-12-29 23:45:51 -0600

    Time: 149 ms (5.63%) | Memory: 44.9 MB (66.20%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var makeEqual = function(words) {
+    
+    let obj = {}
+    for (let word of words) {
+        for (let j = 0; j < word.length; j++) {
+            let letter = word[j]
+            if (obj[letter]) {
+                obj[letter]++
+            } else {
+                obj[letter] = 1
+            }
+        }
+    }
+    for( let key of Object.keys(obj)) {
+        if( obj[key] % words.length !=0 ) return false
+    }
+    
+    return true
+};
