0a64f5419d58d1c4c3770643d00f17aed9fbcd5e
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-01-02 14:03:40 -0600

    Time: 137 ms (15.69%) | Memory: 48.9 MB (31.65%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows === 1 || s.length <= numRows) {
+        return s;
+    }
+
+    let zigzag = [];
+    for (let i = 0; i < numRows; i++) {
+        zigzag[i] = [];
+    }
+    
+    let row = 0;
+    let direction = 1; // 1 represents downward, -1 represents upward
+
+    for (let i = 0; i < s.length; i++) {
+        zigzag[row].push(s[i]);
+
+        if (row === 0) {
+            direction = 1; // Change direction to downward when reaching the top row
+        } else if (row === numRows - 1) {
+            direction = -1; // Change direction to upward when reaching the bottom row
+        }
+
+        row += direction;
+    }
+
+    let result = '';
+    for (let i = 0; i < numRows; i++) {
+        result += zigzag[i].join('');
+    }
+
+    return result;
+};
