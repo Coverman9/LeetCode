32d127c9edfbc68410b8bd1813ab0608527dd156
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-01-03 17:22:21 -0600

    Time: 54 ms (69.35%) | Memory: 42.3 MB (66.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,17 +3,45 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let mp = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    }
     let stack = []
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === "{") {
-            stack.push("}")
-        } else if (s[i] === '(') {
-            stack.push(")")
-        } else if (s[i] === '[') {
-            stack.push("]")
-        } else if (stack.pop() !== s[i]) {
-            return false
+    for (let char of s) {
+        if (char in mp) {
+            stack.push(mp[char])
+        } else {
+            if (stack.pop() !== char) return false
         }
     }
-    return !stack.length
+    return stack.length === 0
+    
+    
+
+    // let obj = {}
+    // for (let el of s) {
+    //     if (el === '(') {
+    //         obj[el] = 1
+    //     } else if (el === '{') {
+    //         obj[el] = 1
+    //     } else if (el === '[') {
+    //         obj[el] = 1
+    //     }
+
+    //     if (el === ')' && obj['(']) {
+    //         obj['(']--
+    //     } else if (el === ']' && obj['[']) {
+    //         obj['[']--
+    //     } else if (el === '}' && obj['{']) {
+    //         obj['{']--
+    //     }
+        
+    // }
+    // let res = Object.values(obj)
+    // for (let tag of res) {
+    //     if (tag>0) return false
+    // }
+    // return true
 };
