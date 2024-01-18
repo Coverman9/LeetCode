49646aef43fe21dba82d94dd9567f1065ceb3334
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-01-18 13:11:19 -0600

    Time: 207 ms (5.06%) | Memory: 52.4 MB (5.29%) - LeetSync

---
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maximumNumberOfStringPairs = function (words) {
+    let word = words.shift()
+    let res = 0
+    let len = words.length
+    for (let i = 0; i < len; i++) {
+        let word1 = word[0] + word[1]
+        let word2 = word[1] + word[0]
+        console.log(word1, word2, words)
+        if (words.includes(word1) || words.includes(word2)) {
+            res++
+        }
+        word = words.shift()
+    }
+    return res
+};
