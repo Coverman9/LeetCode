e943dc3057adb1eeba60a33a38a731d0724410d6
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-02-08 14:08:12 -0600

    Time: 53 ms (92.01%) | Memory: 53.5 MB (11.61%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -5,8 +5,8 @@
 var differenceOfSum = function(nums) {
     let sum = 0
     let dSum = 0
-    sum = nums.reduce((acc, el) => acc + el, 0)
     for (let el of nums) {
+        sum += el
         let num = el.toString().split('')
         for (let dig of num) {
             dSum += +dig
