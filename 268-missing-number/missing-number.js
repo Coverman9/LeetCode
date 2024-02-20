97b677154f48ab89449ab224f87966ae5a7c9af4
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-02-20 13:53:13 -0600

    Time: 276 ms (8.93%) | Memory: 52.1 MB (12.70%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let max = Math.max(...nums)
+    for (let i = 0; i < max; i++) {
+        if (!nums.includes(i)) return i
+    }
+    return max + 1
+};
