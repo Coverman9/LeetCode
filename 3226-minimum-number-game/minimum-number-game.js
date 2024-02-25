0d60a35e413dbb4b91729b1b7c2a70e2906af48f
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-02-25 01:15:20 -0600

    Time: 64 ms (85.17%) | Memory: 52.1 MB (27.94%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.js
+++ 3226-minimum-number-game/minimum-number-game.js
@@ -6,8 +6,7 @@ var numberGame = function(nums) {
     let arr = []
     nums.sort((a,b) => a - b)
     for (let i = 0; i < nums.length; i+=2) {
-        arr.push(nums[i+1])
-        arr.push(nums[i])
+        arr.push(nums[i+1], nums[i])
     }
     return arr
 };
