74212e15aa047ed3176fe5e9c41808b6b7f35437
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-03-12 14:30:51 -0500

    Time: 773 ms (5.00%) | Memory: 57.1 MB (47.09%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.js
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -6,13 +6,6 @@
 var getCommon = function(nums1, nums2) {
     if (nums1[nums1.length - 1] < nums2[0]) return -1
     if (nums2[nums2.length - 1] < nums1[0]) return -1
-    let start = nums1[0] > nums2[0] ? nums1[0] : nums2[0]
-    // console.log(start)
-    // if (nums1.indexOf(start) >= 0) {
-    //     return nums1[nums1.indexOf(start)]
-    // } else {
-    //     return -1
-    // }
     for (let el of nums1) {
         if (nums2.includes(el)) {
             return el
