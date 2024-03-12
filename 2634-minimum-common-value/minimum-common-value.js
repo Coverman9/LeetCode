ab08a8873f842e7775b4a8a820c4af444aca72d1
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-03-12 14:32:33 -0500

    Time: 68 ms (58.69%) | Memory: 56.4 MB (84.95%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.js
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -4,12 +4,25 @@
  * @return {number}
  */
 var getCommon = function(nums1, nums2) {
-    if (nums1[nums1.length - 1] < nums2[0]) return -1
-    if (nums2[nums2.length - 1] < nums1[0]) return -1
-    for (let el of nums1) {
-        if (nums2.includes(el)) {
-            return el
-        } 
+    // if (nums1[nums1.length - 1] < nums2[0]) return -1
+    // if (nums2[nums2.length - 1] < nums1[0]) return -1
+    // for (let el of nums1) {
+    //     if (nums2.includes(el)) {
+    //         return el
+    //     } 
+    // }
+    // return -1
+    let index1 = 0
+    let index2 = 0
+
+    while (index1 < nums1.length && index2 < nums2.length) {
+        let num1 = nums1[index1]
+        let num2 = nums2[index2]
+
+        if (num1 === num2) return num1
+        if (num1 > num2) index2++
+        else index1++
     }
+
     return -1
 };
