e0ab1c26f43fafbbc2911bc21f0f0f880ca6c0d8
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-03-19 02:21:30 -0500

    Time: 85 ms (69.60%) | Memory: 53.9 MB (67.84%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.js
@@ -0,0 +1,24 @@
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function(tasks, n) {
+    var count = {}
+    var longest  = 0
+    
+    // count each task and find longest one
+    for (let c of tasks) {
+        if (count[c] == null) count[c] = 0
+        count[c]++
+        longest = Math.max(longest, count[c])
+    }
+
+    var longestCount = 0
+    // count how many tasks which have longest.
+    for (let c in count){
+        if (count[c] == longest) longestCount++
+    }
+
+    return Math.max((n + 1) * (longest - 1) + longestCount, tasks.length)
+};
