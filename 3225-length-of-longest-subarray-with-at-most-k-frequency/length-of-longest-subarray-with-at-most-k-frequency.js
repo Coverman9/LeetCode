4a6a9b7df12d33d95e544d35642cbb98f85d934f
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-03-28 14:53:30 -0500

    Time: 148 ms (79.46%) | Memory: 70.8 MB (21.43%) - LeetSync

---
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubarrayLength = function (nums, k) {
+    let map = new Map();
+    let left = 0, right = 0;
+    let maxValue = 0;
+    while (right < nums.length) {
+        maxValue = Math.max(maxValue, right - left);
+        map.set(nums[right], map.get(nums[right]) + 1 || 1);
+        while (map.get(nums[right]) > k) {
+            map.set(nums[left], map.get(nums[left]) - 1);
+            left++;
+        }
+        right++;
+    }
+    return Math.max(maxValue, right - left);
+};
