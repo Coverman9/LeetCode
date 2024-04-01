ba94953d903ecf9b1faa0a90b96d2d111863d6ec
Emir Usubaliev <104224490+Coverman9@users.noreply.github.com>
2024-04-01 14:24:10 -0500

    Time: 55 ms (34.87%) | Memory: 48.8 MB (50.18%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,6 +5,6 @@
 var lengthOfLastWord = function(s) {
     let res = s.trim()
     let len = res.split(' ')
-    console.log(len[len.length - 1].length)
+    
     return len[len.length - 1].length
 };
